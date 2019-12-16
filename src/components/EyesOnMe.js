8e68067550e67071187f4fbef6d119cdd1e77378
Cloe Kouadjo <armelfli50@gmail.com>
2019-12-16 00:06:36 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,5 +1,6 @@
 // Code EyesOnMe Component Here
 import React, {Component} from "react"
+<<<<<<< HEAD
 export default class EyesOnMe extends Component{
     focusHandler=()=>{
         console.log("Good!")
@@ -14,3 +15,12 @@ export default class EyesOnMe extends Component{
        return( <div><button onFocus={this.focusHandler} onBlur={this.blurHandler}></button></div>)
     }
 }
+=======
+
+export default class EyesOnMe extends Component{
+  render(){
+    
+   <div>This is a test </div>
+  }
+}
+>>>>>>> 6a205717acc95d632c046750c5cef324b9115ba6
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,5 +1,6 @@
 // Code Keypad Component Here
 import React, {Component} from "react"
+<<<<<<< HEAD
 export default class Keypad extends Component{
     typing(){
         console.log("Entering password...")
@@ -7,4 +8,10 @@ export default class Keypad extends Component{
     render(){
        return( <div><input onKeyUp={this.typing} type= "password"></input></div>)
     }
+=======
+export class Keypad extends Component{
+   render(){
+   return(<div>This is a test </div>)
+  }
+>>>>>>> 6a205717acc95d632c046750c5cef324b9115ba6
 }
